c2e4b6b9d58d1aa360f6f67a049ec962d0430b91
Max Woolf <max@minimaxir.com>
2017-09-24 19:18:56 -0700

    Get GIPHY gif URL

---
+++ utils.py
@@ -0,0 +1,19 @@
+import requests
+import time
+from secrets import *
+
+GIPHY_API_URL = "https://api.giphy.com/v1/gifs/random?api_key={}&tag={}"
+
+
+def get_gif(name, GIPHY_API_KEY=GIPHY_API_KEY, max_attempts=10):
+    attempts = 0
+    while attempts < max_attempts:
+        r = requests.get(GIPHY_API_URL.format(GIPHY_API_KEY, name))
+        gif = r.json()['data']
+        ratio = int(gif['image_width']) / int(gif['image_height'])
+        # return gif['image_mp4_url']
+        # print(ratio)
+        if 1.2 < ratio < 1.6:
+            return gif['image_mp4_url']
+        attempts += 1
+        time.sleep(1)
